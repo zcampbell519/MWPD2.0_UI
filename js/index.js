40db9e332cd5ee1952a11b00f8c0a52b5c46ee8f
Campbell <ZCampbell@helenaschools.org>
2020-05-06 07:55:04 -0600

    initialization

---
+++ js/index.js
@@ -0,0 +1,74 @@
+
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+  
+  if (this.checked) {
+		document.getElementById("myDropdown").classList.toggle("show");
+  
+  }
+    
+}
+
+function myFunction2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+  
+	if (this.checked) {
+		document.getElementById("myDropdown").classList.toggle("show");
+  
+  }
+}
+
+function myFunction3() {
+  document.getElementById("myDropdown3").classList.toggle("show");
+  
+	if (this.checked) {
+		document.getElementById("myDropdown").classList.toggle("show");
+  
+	}
+}
+
+function myFunction4() {
+  document.getElementById("myDropdown4").classList.toggle("show");
+  
+	if (this.checked) {
+		document.getElementById("myDropdown").classList.toggle("show");
+  
+  }
+}
+
+function myFunction5() {
+  document.getElementById("myDropdown5").classList.toggle("show");
+  
+	if (this.checked) {
+		document.getElementById("myDropdown").classList.toggle("show");
+  
+  }
+}
+
+function myFunction6() {
+  document.getElementById("myDropdown6").classList.toggle("show");
+  
+	if (this.checked) {
+		document.getElementById("myDropdown").classList.toggle("show");
+  
+  }
+}
+
+function myFunction7() {
+  document.getElementById("myDropdown7").classList.toggle("show");
+  
+	if (this.checked) {
+		document.getElementById("myDropdown").classList.toggle("show");
+  
+  }
+}
+
+function myFunction8() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
